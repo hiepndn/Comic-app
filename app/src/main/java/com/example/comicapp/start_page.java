788c070d1b8b3b53bf875edeb8a7c852ffa394a8
hiepndn <hninenp@gmail.com>
2025-03-09 18:05:19 +0700

    update

--- app/src/main/java/com/example/comicapp/start_page.java
+++ app/src/main/java/com/example/comicapp/start_page.java
@@ -1,6 +1,8 @@
 package com.example.comicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class start_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(()-> {
+            Intent switchPage = new Intent(this, MainActivity.class);
+            startActivity(switchPage);
+        }, 2000 );
     }
 }
