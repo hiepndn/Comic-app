b32b62e0ecdb59ba00b93ba1ba2d000964c331df
Ngochan-1111 <hanngoc111104.com>
2025-03-12 02:59:48 +0700

    check

--- app/src/main/java/com/example/comicapp/MainActivity.java
+++ app/src/main/java/com/example/comicapp/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.comicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent switchPage = new Intent(MainActivity.this, start_page.class);
                 startActivity(switchPage);
+            }
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.nav_category) {
+                    selectedFragment = new CategoriesFragment();
+                } else if (itemId == R.id.nav_search) {
+                    selectedFragment = new SearchFragment();
+                } else if (itemId == R.id.nav_account) {
+                    selectedFragment = new AccountFragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+                }
+                return true;
             }
         });
 
