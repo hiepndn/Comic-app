56dbc67c8a6a4397d55baca9b2dbe4c0e4f30ae6
hiepndn <hninenp@gmail.com>
2025-03-11 21:50:12 +0700

    update

--- app/src/main/java/com/example/comicapp/MainActivity.java
+++ app/src/main/java/com/example/comicapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.comicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SearchView search;
+        search = findViewById(R.id.searchHome);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchPage = new Intent(MainActivity.this, start_page.class);
+                startActivity(switchPage);
+            }
+        });
+
     }
 }
